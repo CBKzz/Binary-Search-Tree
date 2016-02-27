7c438ff0558f88f3000982b29b55b82667b08f75
CBKzz <baokuncheng@gmail.com>
2016-02-26 21:09:21 -0500

    pretty easy one
    Given a binary search tree and a node in it, find the in-order successor of that node in the BST.

---
+++ inorderSuccessor.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(p.right!=null) return findSmallest(p.right);
+        TreeNode temp=root;
+        Stack<TreeNode> stack=new Stack<>();
+        while(temp!=p){
+            stack.push(temp);
+            temp=p.val<temp.val?temp.left:temp.right;
+        }
+        while(!stack.isEmpty()){
+            TreeNode result=stack.pop();
+            if(result.val>p.val) return result;
+        }
+        return null;
+    }
+    public TreeNode findSmallest(TreeNode root){
+        if(root.left!=null) return findSmallest(root.left);
+        return root;
+    }
+}
